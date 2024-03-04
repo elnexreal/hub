e51320709449de1845fe8a0ed32c4d477fadc057
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-03-04 17:17:49 -0300

    modified tailwind to use Kode Mono

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        Kodemono: ["Kode-Mono", "monospace"]
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
