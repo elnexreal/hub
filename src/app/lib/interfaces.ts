de1d0d423115b89acbcd330cd1d388ce497b4c3f
elnexreal <40328573+elnexreal@users.noreply.github.com>
2024-03-06 21:43:25 -0300

    moved interface file & added response interface

--- src/app/lib/interfaces.ts
+++ src/app/lib/interfaces.ts
@@ -1,8 +1,12 @@
-export default interface repo extends Object {
+export interface repo extends Object {
 	name: string
 	language: string | null
 	html_url: string
 	stargazers_count: number
 	forks: number
 	fork: boolean
+}
+
+export interface response extends Object {
+	data: Array<repo>
 }
